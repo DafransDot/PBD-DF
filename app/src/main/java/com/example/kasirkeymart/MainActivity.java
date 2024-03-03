e112f03ebf4df55156846b321032949b9fd4fd5a
DafransDot <Dafrans05@gmail.com>
2024-03-03 16:49:41 +0700

    DF DOne

--- app/src/main/java/com/example/kasirkeymart/MainActivity.java
+++ app/src/main/java/com/example/kasirkeymart/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.datepicker.MaterialStyledDatePickerDialog;
@@ -16,6 +17,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText etinput;
+
+    private TextView tvBon;
 
     private Button btnInput, btnTotal;
 
@@ -34,16 +37,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnInput = findViewById(R.id.btnInput);
         btnTotal = findViewById(R.id.btnTotal);
         rbMembership = findViewById(R.id.rbmembership);
+        tvBon = findViewById((R.id.tvBon));
 
         btnInput.setOnClickListener(this);
         btnTotal.setOnClickListener(this);
 
         menu = new ArrayList<>();
-        menu.add(new barang(1, "kasur", 100000 + 500));
-        menu.add(new barang(2, "Meja", 75000 + 500));
-        menu.add(new barang(3, "kursi", 50000 + 500));
-        menu.add(new barang(4, "wastafel", 120000 + 500));
-        menu.add(new barang(5, "granit", 25000 + 500));
+        menu.add(new barang(1, "kasur", 100000 ));
+        menu.add(new barang(2, "Meja", 75000 ));
+        menu.add(new barang(3, "kursi", 50000 ));
+        menu.add(new barang(4, "wastafel", 120000 ));
+        menu.add(new barang(5, "granit", 25000 ));
 
         pesanan = new ArrayList<>();
 
@@ -78,28 +82,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(this, "pesanan ditambahkan "+ selectedbarang.getNama(), Toast.LENGTH_SHORT).show();
                 etinput.setText("");
             } else {
-                Toast.makeText(this, "ID makanan tidak valid.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "ID pesanan tidak valid.", Toast.LENGTH_SHORT).show();
             }
         } catch (NumberFormatException e) {
-            Toast.makeText(this, "Masukkan ID makanan yang valid.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Masukkan ID pesanan yang valid.", Toast.LENGTH_SHORT).show();
         }
     }
     private void calculateTotal() {
         double total = 0;
+        double potongan = 0;
         for ( barang brg : pesanan) {
-            total += brg.getHarga() + 2000 ;
+            total += brg.getHarga() + 500 ;
         }
 
         if (rbMembership.isChecked()) {
-            double potongan = total * 0.05;
+            potongan = total * 0.05;
             total -= potongan;
         }
 
-        showReceipt(total);
+        showReceipt(total, potongan);
     }
 
-    private void showReceipt(double total) {
+    private void showReceipt(double total, double potongan) {
+        StringBuilder strukText = new StringBuilder("\bTransaksi Anda\b\n");
 
+        if (!pesanan.isEmpty() ) {
+            for (barang barang : pesanan) {
+                strukText.append(barang.getNama()).append(" - Rp").append(barang.getHarga()).append("\n");
+            }
+        } else {
+            strukText.append("Tidak ada pesanan.");
+        }
 
+        strukText.append("\nDengan Admin perbarang Rp.500/barang \n dengan potongan Membership : Rp. "+potongan +"\nTotal Pembayaran: Rp").append(total);
+
+        // Set teks struk ke dalam TextView atau komponen yang sesuai
+        tvBon.setText(strukText.toString());
+
+        // Reset pesanan setelah menampilkan struk
+        pesanan.clear();
     }
 }
